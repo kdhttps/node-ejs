eda710d3d7356a474b80c9f068daa8000f99f136
kdhttps <kdhttps@gmail.com>
2019-04-30 22:50:29 +0530

    add ejs demo

---
+++ README.md
@@ -0,0 +1,19 @@
+# node-ejs
+
+Node JS ejs template engine demo
+
+## Installation
+
+Install dependencies
+
+```
+$ npm install
+```
+
+# Configuration
+
+Use .env file for configuration.
+
+# License
+
+MIT
---
+++ app.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+
+// set up view engine
+app.set('view engine', 'ejs');
+
+// endpoints
+app.get('/logout', (req, res) => {
+  res.render('logout');
+});
+
+app.get('/todos', (req, res) => {
+  res.render('todos');
+});
+
+app.get('/users', (req, res) => {
+  res.render('users');
+});
+
+app.get('/home', (req, res) => {
+  res.render('home');
+});
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.use((err, req, res, next) => {
+  console.log(err.stack);
+  if (err) {
+    res.status(500).send({ Error: err.stack });
+  }
+});
+
+// Start server
+server.listen(process.env.PORT, () => {
+  console.log(`-----------------------\nServer started successfully!, Open this URL ${process.env.BASE_URL}\n-----------------------`);
+});
